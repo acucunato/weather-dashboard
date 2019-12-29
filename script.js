dd92747cf54491a88348a7588b3c762ec0e22c71
Alyssa Cucunato <acucunato18@gmail.com>
2019-12-28 21:31:02 -0500

    added basic style and pseudo coded js

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+// make ajax call for current conditions
+// local storage for search history/cities list
+// ajax call for 5 day forecast 
+
+//functions --
+// current conditions
+//   display - current temp, current humidity, windspeed, UV index
+// 5 day forecast
+// search history
+// uv index
+
+
+
+
+
+})
